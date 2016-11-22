4cd7df9837f93cffb781ea473f4c71b8dc08db1e
vinilmtuity <vinil.satrasala@mtuity.com>
2016-11-22 15:23:22 +0530

    resolved crash and modified

--- app/src/main/java/com/android/citybustickets/GenerateTicketActivity.java
+++ app/src/main/java/com/android/citybustickets/GenerateTicketActivity.java
@@ -23,7 +23,7 @@ public class GenerateTicketActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_generate_ticket);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
--- app/src/main/java/com/android/citybustickets/WriteTextPlainTagActivity.java
+++ app/src/main/java/com/android/citybustickets/WriteTextPlainTagActivity.java
@@ -14,6 +14,7 @@ import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -50,8 +51,23 @@ public class WriteTextPlainTagActivity extends AppCompatActivity implements View
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_text_plain_tag);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         prepareNfcAdapter();
         fillUi();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent callingHomeIntent = new Intent(WriteTextPlainTagActivity.this, MainActivity.class);
+                callingHomeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(callingHomeIntent);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
